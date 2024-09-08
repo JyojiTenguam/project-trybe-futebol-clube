e4557558a9d1ace2906a5b86f1daa86ec7586574
JyojiTenguam <j.y.oji@hotmail.com>
2024-09-08 20:21:53 -0300

    Resolvendo testes de Trybe Futebol Clube

---
+++ app/backend/src/mocks/team.mock.ts
@@ -0,0 +1,16 @@
+export const teamMock = { id: 1, teamName: 'Avaí/Kindermann' };
+
+export const teamsMock = [
+  {
+    id: 1,
+    teamName: 'Avaí/Kindermann',
+  },
+  {
+    id: 2,
+    teamName: 'Bahia',
+  },
+  {
+    id: 3,
+    teamName: 'Botafogo',
+  },
+];
---
+++ app/backend/src/tests/Teams.test.ts
@@ -0,0 +1,39 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { App } from '../app';
+import TeamsModel from '../database/models/teams.model';
+import { teamMock, teamsMock } from '../mocks/team.mock';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+const { expect } = chai;
+
+describe('Teams testes', function(){
+  let findAllStub: sinon.SinonStub;
+  let findByPkStub: sinon.SinonStub;
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Verifique se a lista de todos os times está completa', async function(){
+    findAllStub = sinon.stub(TeamsModel, 'findAll').resolves(teamsMock as any);
+
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(teamsMock);
+  });
+
+  it('Confirme se a equipe associada a uma ID específica está sendo retornada', async function(){
+    findByPkStub = sinon.stub(TeamsModel, 'findByPk').resolves(teamMock as any);
+
+    const response = await chai.request(app).get('/teams/1');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(teamMock);
+  });
+});
